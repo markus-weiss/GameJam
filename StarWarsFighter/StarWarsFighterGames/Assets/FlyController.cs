82fdd9f13812032a59ae035b4d7f3091a0a5b9c9
DESKTOP-E8OEJA7\schnu <schnubbi2704@web.de>
2018-04-22 22:39:22 +0200

    asdgf

---
+++ StarWarsFighter/StarWarsFighterGames/Assets/FlyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyController : MonoBehaviour {
+
+    float shove = 0;
+    public float acceleration = 0.1f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        /*
+        if (shove > 0)
+        {
+            shove = 0;
+        }
+        else if (shove < 1)
+        {
+            shove = 1;
+        }
+        */
+
+        if (Input.KeyPressed(KeyCode.W))
+        {
+            shove += acceleration;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            shove -= acceleration;
+        }
+
+        Debug.Log(shove);
+    }
+}
